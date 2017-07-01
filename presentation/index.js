feb03ac35114dbce1fd9662294e27afa86e9bd07
xavcz <xavier.cazalot@gmail.com>
2017-07-01 15:43:50 -0700

    Make it work! \o/

--- presentation/index.js
+++ presentation/index.js
@@ -1,20 +1,137 @@
 import React from "react";
+
 import {
   ApolloProvider,
   ApolloClient,
-  createNetworkInterface
+  createNetworkInterface,
+  graphql
 } from "react-apollo";
 
-import CodeTour from "./CodeTour";
+// Import Spectacle Core tags
+import {
+  Deck,
+  Heading,
+  ListItem,
+  List,
+  Slide,
+  Text
+} from "spectacle";
+
+import CodeSlide from "spectacle-code-slide";
+
+// fancy!
+import TOUR_QUERY from "./tour.graphql";
+
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+// Require CSS
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
+
+const theme = createTheme(
+  {
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#03A9FC",
+    quartenary: "#122b45"
+  },
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  }
+);
+
+const extractRepoInfo = (fullName) => fullName.split("/");
+
+const CodeTour = ({ tour, loading /*, error */ }) => {
+  if (loading) {
+    return (
+      <Deck transition={[]} transitionDuration={0} theme={theme}>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Loading your tour...
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            \o/
+          </Text>
+        </Slide>
+      </Deck>
+    );
+  }
+
+  const { targetRepository, repository, description, steps } = tour;
+
+  const [, targetName] = extractRepoInfo(targetRepository);
+  const [ author ] = extractRepoInfo(repository);
+
+  return (
+    <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+      {/* welcome slide */}
+      <Slide transition={["fade"]} bgColor="primary">
+        <Heading size={3} lineHeight={1} textColor="secondary">
+          {targetName}
+        </Heading>
+        <Text margin="10px 0 0" textColor="tertiary" size={1} caps>
+          {description}
+        </Text>
+      </Slide>
+      {/* steps overview */}
+      <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+        <Heading size={6} textColor="secondary" caps>Steps</Heading>
+        <List ordered>
+          {steps.map(({ _id, title }) => (
+            <ListItem key={`item-${_id}`}>{title}</ListItem>
+          ))}
+        </List>
+      </Slide>
+      {/* for each step, add a code slide with the title & code sections */}
+      {steps
+        .map(({ _id, title, code, sections }, index) => [
+          <CodeSlide
+            key={`step-${_id}-section-${index}`}
+            transition={[]}
+            lang="js"
+            code={code}
+            bgColor="quartenary"
+            ranges={[
+              { loc: [0, 1], title },
+              ...sections.map((section) => ({
+                loc: [section.lineStart - 1, section.lineEnd],
+                note: section.slug
+              }))]}
+          />
+        ])}
+      {/* conclusion slide */}
+      <Slide transition={["fade"]} bgColor="primary">
+        <Heading size={3} textColor="tertiary">
+          Thanks for taking this tour!
+        </Heading>
+        <Heading size={5} textColor="quartenary">
+          Tour made by {author}
+        </Heading>
+      </Slide>
+    </Deck>
+  );
+};
+
+const CodeTourWithData = graphql(TOUR_QUERY, {
+  options: ({ tourRepository }) => ({ variables: { tourRepository } }),
+  props: ({ data: { tour, loading, error } }) => ({
+    tour,
+    loading,
+    error
+  })
+})(CodeTour);
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
-    uri: "http://localhost:3000/graphql" // === CodeTours GraphQL server
+    uri: "https://ljk3vj7pq.lp.gql.zone/graphql" // === CodeTours GraphQL server
   })
 });
 
 export default () => (
   <ApolloProvider client={client}>
-    <CodeTour tourRepository="partyparrot/codetours-starter-kit" />
+    <CodeTourWithData tourRepository="xavcz/codetours-starter-kit-v2" />
   </ApolloProvider>
 );
